5dadc19281864036a733858d1c1db3588551828f
VolTorian <33360939+VolTorian@users.noreply.github.com>
2026-01-15 01:52:10 -0800

    post basic functionality working

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const newMessageRouter = require("./routes/newMessageRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -5,4 +5,8 @@ newMessageRouter.get("/", (req, res) => {
     res.render("form");
 });
 
+newMessageRouter.post("/", (req, res) => {
+    res.send(`${req.body.name}: ${req.body.message}`);
+});
+
 module.exports = newMessageRouter;
