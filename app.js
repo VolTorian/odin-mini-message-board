ac4800a3bff361e900339930e3a557ee4ed8c2aa
VolTorian <33360939+VolTorian@users.noreply.github.com>
2026-01-21 17:56:56 -0800

    button anchor element to link to /new route

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 const newMessageRouter = require("./routes/newMessageRouter");
+const assetsPath = path.join(__dirname, "public");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
