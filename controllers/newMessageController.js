4a6d56ac1d0206987940b5417a31f3a9b0fcd749
VolTorian <33360939+VolTorian@users.noreply.github.com>
2026-03-09 16:48:25 -0700

    validates name and message errors
    error messages for different fields show in different parts of view

--- controllers/newMessageController.js
+++ controllers/newMessageController.js
@@ -8,19 +8,20 @@ const validateName = [
 
 const validateMessage = [
     body("message").trim()
-        .matches(/^[a-zA-Z0-9_]+$/).withMessage("Message must only contain letters, numbers, and underscores")
-        .isLength({ min: 3, max: 2000 }).withMessage("Message must be between 3 and 2000 characters")
+        .isLength({ min: 1, max: 500 }).withMessage("Message must be between 1 and 500 characters")
 ]
 
 messageCreatePost = [
     validateName,
     validateMessage,
     async (req, res) => {
-        console.log("test")
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            console.log(errors.array())
-            return res.status(400).render("form");
+            // console.log(errors.array())
+            return res.status(400).render("form", {
+                nameErrors: errors.array().filter((error) => error.path === "name"),
+                messageErrors: errors.array().filter((error) => error.path === "message"),
+            });
         }
         const { name } = matchedData(req);
         console.log("test username " + name);
