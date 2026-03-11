a29a3be3b3551f537e276a3bf8fa80bef266a6a9
VolTorian <33360939+VolTorian@users.noreply.github.com>
2026-03-11 16:20:05 -0700

    db table initial creation and seeding script

---
+++ db/populatedb.js
@@ -0,0 +1,28 @@
+const { Client } = require("pg");
+
+const SQL = `
+    CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR (255),
+    message TEXT,
+    timestamp TIMESTAMPTZ
+    );
+    
+    INSERT INTO messages (name, message, timestamp)
+    VALUES
+        ('First_Seed', 'Testing first seeded message!', '${new Date().toISOString()}'),
+        ('Seed2', 'Testing 2nd seeded message!', '${new Date().toISOString()}');
+`;
+
+async function main() {
+    console.log("seeding...");
+    const client = new Client({
+        connectionString: `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PSQL_PORT}/${process.env.DATABASE}`,
+    });
+    await client.connect();
+    await client.query(SQL);
+    await client.end();
+    console.log("seeding done");
+}
+
+main();
